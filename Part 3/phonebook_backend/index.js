628ed9a7360e5463745437b48d3dd50d68287709
Ha My Nguyen <my.1.nguyen@aalto.fi>
2025-12-21 23:56:36 +0200

    3.4 to 3.6  completed

--- Part 3/phonebook_backend/index.js
+++ Part 3/phonebook_backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-console.log("very beginning")
+app.use(express.json())
 
 let persons = [
     { 
@@ -22,28 +22,58 @@ let persons = [
       "id": "4",
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
-    }
+    },
+    { 
+      "id": "5",
+      "name": "Arto Bloop", 
+      "number": "040-123456"
+    },
 ]
 
 app.get('/api/persons', (request, response) => {response.json(persons)})
 
-app.get('/info', (request, response) => 
-    {
-        response.send(`<p>Phonebook has info of ${persons.length} people</p>
-                       <p>${new Date().toString()}</p>`)
-    })
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info of ${persons.length} people</p>
+                    <p>${new Date().toString()}</p>`)
+})
 
 app.get('/api/persons/:id', (req, res) => {
     const id = req.params.id
-    console.log(`${id}`)
     const person = persons.find(person => person.id === id)
-    console.log(`${person}`)
-
     if (!person) 
         {return res.status(404).json({error: "Person not found"})}
     res.json(person)
 })
 
+app.delete('/api/persons/:id', (req, res) =>{
+    const id = req.params.id
+    const person = persons.find(person => person.id === id)
+    if (!person) 
+        {return res.status(404).json({error: "Person not found"})}
+    persons = persons.filter(person => person.id !== id)
+    res.status(200).json({deleted: `Person ${id} has been deleted`})
+})
+
+
+app.post('/api/persons/', (req, res) => {
+    const body = req.body
+    console.log("new person entry is coming")
+
+    if (!body.name.trim() || !body.number.trim()){
+        return res.status(400).json({error: "name or number is missing"})
+    }
+    if (persons.find(p => p.name === body.name)){
+        return res.status(400).json({ error: "name must be unique"})
+    }
+    const new_person = {
+        id : String(Math.floor(Math.random() * 1000000)),
+        name : body.name,
+        number : body.number
+    }
+    persons = persons.concat(new_person)
+    res.json(new_person)
+    console.log("person added")
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
