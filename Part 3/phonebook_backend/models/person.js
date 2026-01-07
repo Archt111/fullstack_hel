0ebeef33fdd9842e8496b47494d15d5b3db340d1
Ha My Nguyen <my.1.nguyen@aalto.fi>
2026-01-07 18:51:46 +0200

    fly can connect to mongo

---
+++ Part 3/phonebook_backend/models/person.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+mongoose.set('strictQuery',false)
+
+
+const url = process.env.MONGODB_URI
+mongoose.connect(url, {family:4})
+    .then(result => {
+        console.log("connected to mongoDB")})
+    .catch(error => {
+        console.log("error connecting: ", error.message)
+    })
+
+const personSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  number: {type: String, required: true}
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
--- Part 3/phonebook_backend/mongo.js
+++ Part 3/phonebook_backend/mongo.js
@@ -1,47 +1,42 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+const  [, , password, new_name, new_phone] = process.argv
+if (!password){
+  console.log('Password required!')
+  process.exit(1)
 }
 
-const password = process.argv[2];
-const new_name = process.argv[3] ?? null;
-const new_phone = process.argv[4] ?? null;
-
-// Add a DB name: phonebook
-const url = `mongodb+srv://fullstack:${password}@clusterfso.oekfcyf.mongodb.net/phonebook?retryWrites=true&w=majority&appName=ClusterFSO`;
-
-mongoose.set("strictQuery", false);
+const url = `mongodb+srv://fullstack:${password}@clusterfso.oekfcyf.mongodb.net/phonebook?appName=ClusterFSO`
+mongoose.set('strictQuery',false)
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
-const Person = mongoose.model("PB", personSchema);
+  name: {type: String, require: true},
+  number: {type: String, require: true}
+})
+const Person = mongoose.model('Person', personSchema)
+personSchema.index({name: 1}, {unique: true})
 
-async function main() {
-  try {
-    await mongoose.connect(url, {
-      family: 4,
-      serverSelectionTimeoutMS: 20000,
-    });
+// second arg is for settings, in this case is to set IP type to be IPv4
+mongoose.connect(url, { family: 4 })
 
-    if (!new_name && !new_phone) {
-      console.log("phonebook:");
-      const persons = await Person.find({});
-      persons.forEach((p) => console.log(`${p.name} ${p.number}`));
-    } else {
-      const person = new Person({ name: new_name, number: new_phone });
-      await person.save();
-      console.log(`added ${new_name} number ${new_phone} to phonebook`);
-    }
-  } catch (err) {
-    console.error("Mongo error:", err);
-    process.exitCode = 1;
-  } finally {
-    await mongoose.connection.close();
-  }
+// No name, no phone
+if (!new_name && !new_phone) {
+  console.log('phonebook:')
+  Person.find({}).then(persons => {
+    persons.forEach(p => console.log(`${p.name} ${p.number}`))
+    mongoose.connection.close()
+  })
+  return
 }
 
-main();
+if (!new_name || !new_phone) {
+  console.log('need both name and phone to add entry')
+  process.exit(1)
+}
+
+// New name and phone
+const person = new Person({name: new_name, number: new_phone})
+person.save().then(result => {
+  console.log(`added ${new_name} number ${new_phone} to phonebook`)
+  mongoose.connection.close()
+})
