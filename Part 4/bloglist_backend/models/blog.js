cb3ecd1c281ad13853b655626f40fed58901278b
Ha My Nguyen <my.1.nguyen@aalto.fi>
2026-04-27 09:09:40 +0300

    4.9-4.12

--- Part 4/bloglist_backend/models/blog.js
+++ Part 4/bloglist_backend/models/blog.js
@@ -4,7 +4,14 @@ const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number,
+  likes: {type: Number, default: 0},
 })
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString()
+    delete returnedObj._id
+    delete returnedObj.__v
+  }
+})
 module.exports = mongoose.model('Blog', blogSchema)
--- Part 4/bloglist_backend/tests/blog_api.test.js
+++ Part 4/bloglist_backend/tests/blog_api.test.js
@@ -21,6 +21,7 @@ const testBlogs = [
   }
 ]
 
+// 4.8
 beforeEach(async () => {
   await blog.deleteMany({})
   await blog.insertMany(testBlogs)
@@ -36,6 +37,43 @@ test('blogs are returned as json', async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
   assert.strictEqual(response.body.length, testBlogs.length)
+})
+
+// 4.9 
+test('unique id', async() => {
+  const res = await api.get('/api/blogs')
+  assert.ok(res.body[0].id)
+  assert.strictEqual(res.body[0]._id, undefined)
+})
+
+// 4.10
+test('create post ok', async() => {
+  const newBlog = { title: 'Third blog', author: 'Charlie', url: 'http://example.com/3', likes: 3 }
+  await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/)
+  
+  const res = await api.get('/api/blogs')
+  assert.strictEqual(res.body.length, testBlogs.length+1)
+  assert.ok(res.body.map(b => b.title).includes('Third blog'))
+
+})
+
+// 4.11 
+test('missing likes def to 0', async () => {
+    const newBlog = { title: 'No likes blog', author: 'Dave', url: 'http://example.com/4' }
+    const response = await api.post('/api/blogs').send(newBlog).expect(201)
+
+    assert.strictEqual(response.body.likes, 0)
+  })
+  
+// 4.12
+test('blog without title is 400', async () => {
+  const newBlog = { author: 'Eve', url: 'http://example.com/5', likes: 1 }
+  await api.post('/api/blogs').send(newBlog).expect(400)
+})
+
+test('blog without url is 400', async () => {
+  const newBlog = { title: 'No url blog', author: 'Eve', likes: 1 }
+  await api.post('/api/blogs').send(newBlog).expect(400)
 })
 
 after(async () => {
