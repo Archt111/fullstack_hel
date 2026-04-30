9ab300fbff774a55bcb5700920c1bc05cd3f1301
Ha My Nguyen <my.1.nguyen@aalto.fi>
2026-04-30 09:14:20 +0300

    part a done with first notes of the course made

--- Part 5/bloglist-frontend/src/App.jsx
+++ Part 5/bloglist-frontend/src/App.jsx
@@ -9,20 +9,53 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
-  useEffect(() => { blogService.getAll().then(blogs =>  setBlogs(blogs))}, [])
+  useEffect(() => { 
+    blogService.getAll().then(blogs => setBlogs(blogs))
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const loggedUser = JSON.parse(loggedUserJSON)
+      setUser(loggedUser)
+      blogService.setToken(loggedUser.token)
+    }
+  }, [])
 
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const loggedUser = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(loggedUser))
+      
+      blogService.setToken(loggedUser.token)
       setUser(loggedUser)
       setUsername('')
       setPassword('')
     } catch {
-        setErrorMessage('wrong credentials')
-        setTimeout(() => {
-        setErrorMessage(null)}, 5000)
+      setErrorMessage('wrong credentials')
+      setTimeout(() => setErrorMessage(null), 5000)
+    }
+  }
+
+  const handleCreate = async (event) => {
+    event.preventDefault()
+    try {
+      const newBlog = await blogService.create({ title, author, url })
+      setBlogs(blogs.concat(newBlog))
+      setSuccessMessage(`a new blog '${newBlog.title}' by ${newBlog.author} added`)
+      setTimeout(() => setSuccessMessage(null), 5000)
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch {
+      setErrorMessage('failed to create blog')
+      setTimeout(() => setErrorMessage(null), 5000)
     }
   }
 
@@ -30,11 +63,19 @@ const App = () => {
     <form onSubmit={handleLogin}>
       <div>
         username
-        <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+        <input
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
       </div>
       <div>
         password
-        <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
+        <input
+          type="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
       </div>
       <button type="submit">login</button>
     </form>
@@ -44,15 +85,30 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in</p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <form onSubmit={handleCreate}>
+        <div>
+          title
+          <input value={title} onChange={({ target }) => setTitle(target.value)} />
+        </div>
+        <div>
+          author
+          <input value={author} onChange={({ target }) => setAuthor(target.value)} />
+        </div>
+        <div>
+          url
+          <input value={url} onChange={({ target }) => setUrl(target.value)} />
+        </div>
+        <button type="submit">create</button>
+      </form>
+      {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
     </div>
   )
 
   return (
     <div>
       <h2>log in to application</h2>
+      {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
       {!user && loginForm()}
       {user && blogList()}
     </div>
