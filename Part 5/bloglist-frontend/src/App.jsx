2e311d3d04b887acb1bd1b693a994d0e4dcb9b5b
Ha My Nguyen <my.1.nguyen@aalto.fi>
2026-05-10 13:30:29 +0300

    UI upgraded -5.26

--- Part 5/bloglist-frontend/src/App.jsx
+++ Part 5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import BlogForm from './components/BlogForm'
-import Togglable from './components/Togglable'
+import { Link, Navigate, Route, Routes, useMatch, useNavigate } from 'react-router-dom'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -12,9 +12,13 @@ const App = () => {
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const [successMessage, setSuccessMessage] = useState(null)
+  const navigate = useNavigate()
 
   useEffect(() => {
-    blogService.getAll().then(blogs => setBlogs(blogs))
+    blogService.getAll().then(blogs => {
+      console.log('log blogs loaded', blogs.length)
+      setBlogs(blogs)
+    })
   }, [])
 
   useEffect(() => {
@@ -36,10 +40,21 @@ const App = () => {
       setUser(loggedUser)
       setUsername('')
       setPassword('')
+      console.log('log login ok')
+      navigate('/')
     } catch {
+      console.log('log login fail')
       setErrorMessage('wrong credentials')
       setTimeout(() => setErrorMessage(null), 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    blogService.setToken(null)
+    setUser(null)
+    console.log('log logout')
+    navigate('/')
   }
 
   const createBlog = async (blogObject) => {
@@ -48,56 +63,59 @@ const App = () => {
       setBlogs(blogs.concat(newBlog))
       setSuccessMessage(`a new blog '${newBlog.title}' by ${newBlog.author} added`)
       setTimeout(() => setSuccessMessage(null), 5000)
+      console.log('log create ok', newBlog.id)
     } catch {
       setErrorMessage('failed to create blog')
       setTimeout(() => setErrorMessage(null), 5000)
+      console.log('log create fail')
     }
   }
 
   const handleLike = async (blog) => {
-    const updatedBlog = await blogService.update(blog.id, {
+    console.log('log like try', blog.id)
+    const updatedBlog = {
       ...blog,
       likes: blog.likes + 1,
       user: blog.user?.id || blog.user?._id || blog.user,
-    })
-
-    setBlogs(blogs.map(currentBlog => currentBlog.id === blog.id ? updatedBlog : currentBlog))
+    }
+    const returnedBlog = await blogService.update(blog.id, updatedBlog)
+    setBlogs(blogs.map(b => b.id === blog.id ? returnedBlog : b))
+    console.log('log like ok', blog.id)
   }
 
   const handleDelete = async (blog) => {
-    if (!window.confirm(`Remove blog '${blog.title}' by ${blog.author}?`)) return
-
-    try {
+    if (window.confirm(`Remove blog '${blog.title}' by ${blog.author}?`)) {
+      console.log('log delete try', blog.id)
       await blogService.remove(blog.id)
       setBlogs(blogs.filter(b => b.id !== blog.id))
-      setSuccessMessage(`blog '${blog.title}' removed`)
-      setTimeout(() => setSuccessMessage(null), 5000)
-    } catch {
-      setErrorMessage('failed to delete blog')
-      setTimeout(() => setErrorMessage(null), 5000)
+      console.log('log delete ok', blog.id)
+      navigate('/')
     }
   }
 
   const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
   )
 
   const blogList = () => {
@@ -105,22 +123,67 @@ const App = () => {
     return (
       <div>
         <h2>blogs</h2>
-        <p>{user.name} logged in</p>
-        <Togglable buttonLabel="create new blog">
-          <BlogForm createBlog={createBlog} />
-        </Togglable>
-        {sortedBlogs.map(blog => <Blog key={blog.id} blog={blog} handleLike={handleLike} handleDelete={handleDelete} user={user} />)}
+        {user && <p>{user.name} logged in</p>}
+        {sortedBlogs.map(blog => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            handleLike={handleLike}
+            handleDelete={handleDelete}
+            user={user}
+          />
+        ))}
+      </div>
+    )
+  }
+
+  const createBlogPage = () => (
+    <div>
+      <BlogForm createBlog={createBlog} />
+    </div>
+  )
+
+  const blogMatch = useMatch('/blogs/:id')
+  const matchedBlog = blogMatch ? blogs.find((blog) => blog.id === blogMatch.params.id) : null
+
+  const singleBlogView = () => {
+    if (!matchedBlog) return null
+    const isOwnBlog = user && matchedBlog.user && (user.id === matchedBlog.user.id || user.id === matchedBlog.user._id)
+
+    return (
+      <div>
+        <h2>{matchedBlog.title}: {matchedBlog.author}</h2>
+        <div>
+          <a href={matchedBlog.url} target="_blank" rel="noreferrer">{matchedBlog.url}</a>
+        </div>
+        <div>
+          likes {matchedBlog.likes}
+          {user && <button type="button" onClick={() => handleLike(matchedBlog)}>like</button>}
+        </div>
+        <div>Added by {matchedBlog.user?.name}</div>
+        {isOwnBlog && <button type="button" onClick={() => handleDelete(matchedBlog)}>remove</button>}
       </div>
     )
   }
 
   return (
     <div>
-      <h2>log in to application</h2>
+      <div style={{ marginBottom: 10 }}>
+        <Link style={{ marginRight: 10 }} to="/">blogs</Link>
+        {!user && <Link to="/login">login</Link>}
+        {user && <Link style={{ marginRight: 10 }} to="/create">new blog</Link>}
+        {user && <button onClick={handleLogout}>logout</button>}
+      </div>
+
       {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
       {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-      {!user && loginForm()}
-      {user && blogList()}
+
+      <Routes>
+        <Route path="/" element={blogList()} />
+        <Route path="/login" element={user ? <Navigate to="/" replace /> : loginForm()} />
+        <Route path="/create" element={user ? createBlogPage() : <Navigate to="/login" replace />} />
+        <Route path="/blogs/:id" element={singleBlogView()} />
+      </Routes>
     </div>
   )
 }
--- Part 5/bloglist-frontend/src/components/Blog.jsx
+++ Part 5/bloglist-frontend/src/components/Blog.jsx
@@ -1,32 +1,32 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Blog = ({ blog, handleLike, handleDelete, user }) => {
-  const [detailsVisible, setDetailsVisible] = useState(false)
-
-  const blogStyle = {
+const Blog = ({ blog, handleLike, handleDelete, user, isSingleView = false }) => {
+  const [detailsVisible, setDetailsVisible] = useState(isSingleView)
+/*
+const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
-    border: 'solid',
+    //border: 'solid',
     borderWidth: 1,
     marginBottom: 5,
   }
-
-  const toggleDetails = () => { setDetailsVisible(!detailsVisible)}
+    
+const toggleDetails = () => { setDetailsVisible(!detailsVisible)}
+*/
 
   const isOwnBlog = user && blog.user && (user.id === blog.user.id || user.id === blog.user._id)
-
   return (
-    <div style={blogStyle}>
+    <div>
       <div>
-        {blog.title} {blog.author}
-        <button onClick={toggleDetails}>{detailsVisible ? 'hide' : 'view'}</button>
+        <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
       </div>
       {detailsVisible && (
         <div>
           <div>{blog.url}</div>
           <div>
             likes {blog.likes}
-            <button type="button" onClick={() => handleLike(blog)}>like</button>
+            {user && <button type="button" onClick={() => handleLike(blog)}>like</button>}
           </div>
           <div>{blog.user?.name}</div>
           {isOwnBlog && <button type="button" onClick={() => handleDelete(blog)}>remove</button>}
